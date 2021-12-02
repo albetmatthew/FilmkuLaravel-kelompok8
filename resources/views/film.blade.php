9310c438862fdcfa3a7f8cb325e9f184420c58f2
ifs19045 <josuagaolus72@gmail.com>
2021-12-03 06:39:51 +0700

    film.blade
    send film

---
+++ resources/views/film.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+
+    <title>Deskripsi MIFILM</title>
+    <meta content="" name="description" />
+    <meta content="" name="keywords" />
+
+    <!-- Favicons -->
+    <link href="assets/img/mifilm-logo.jpg" rel="icon" />
+    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
+
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+    <link href='https://fonts.googleapis.com/css?family=Oswald:300,400,500,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet" />
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet" />
+    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet" />
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet" />
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
+
+    <!-- Template Main CSS File -->
+ 
+ 
+    <link rel="stylesheet" type="text/css" href="css/theme.css">
+  </head>
+
+  <body>
+
+    <!-- ======= Header ======= -->
+    <header id="header" class="fixed-top d-flex align-items-cente">
+        <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+          <h1 class="logo me-auto me-lg-0"><a href="index.html">MiFilm</a></h1>
+  
+          <nav id="navbar" class="navbar order-last order-lg-0">
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav>
+          <!-- .navbar -->
+          <div class="d-flex">
+            <a href="#suggestion" class="suggestion-btn scrollto d-none d-lg-flex">Masuk</a>
+            <a href="#suggestion" class="suggestion-btn scrollto d-none d-lg-flex">Daftar</a>
+          </div>
+        </div>
+      </header>
+    
+      <!-- ======= Hero Section ======= -->
+      <section id="hero" class="d-flex align-items-center">
+        <div class="container position-relative text-center text-lg-start" data-aos="zoom-in" data-aos-delay="100">
+          <div class="row">
+            <div class="col-lg-8">
+                <h1 class="section-title">Movies info</h1>
+                <div class="page-breadcrumbs">
+                    <a class="content-link" href="#">Home</a>
+                    <span class="text-theme mx-2"><i class="fas fa-chevron-right"></i></span>
+                    <a class="content-link" href="#">Movies</a>
+            </div>
+            <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
+              <a href="https://www.youtube.com/watch?v=GlrxcuEDyF8" class="glightbox play-btn"></a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End Hero -->
+  
+
+        
+          <div class="container">
+            <div class="sidebar-container">
+                <div class="content">
+                    <section class="section-long">
+                        <div class="section-line">
+                            <div class="movie-info-entity">
+                                <div class="entity-poster" data-role="hover-wrap">
+                                    <div class="embed-responsive embed-responsive-poster">
+                                        <img class="embed-responsive-item" src="img/Blick.jpg" alt="" width="370px" height="450" />
+                                    </div>
+                                    <div class="d-over bg-theme-lighted collapse animated faster" data-show-class="fadeIn show" data-hide-class="fadeOut show">
+                                        
+                                        <div class="entity-play">
+                                            <a class="action-icon-theme action-icon-bordered rounded-circle" href="https://www.youtube.com/watch?v=d96cjJhvlMA" data-magnific-popup="iframe">
+                                                <span class="icon-content"><i class="fas fa-play"></i></span>
+                                            </a>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                                
+                                <div class="entity-content ">
+                                    <h2 class="entity-title">Blick</h2>
+                                    <div class="entity-category">
+                                        <a class="content-link" href="movies-blocks.html">comedy</a>,
+                                        <a class="content-link" href="movies-blocks.html">detective</a>
+                                    </div>
+                                   
+                                    <div class="entity-info">
+                                        <div class="info-lines">
+                                            <div class="info info-short">
+                                                <span class="text-theme info-icon"><i class="fas fa-star"></i></span>
+                                                <span class="info-text">8,7</span>
+                                                <span class="info-rest">/10</span>
+                                            </div>
+                                            <div class="info info-short">
+                                                <span class="text-theme info-icon"><i class="fas fa-clock"></i></span>
+                                                <span class="info-text">130</span>
+                                                <span class="info-rest">&nbsp;min</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <ul class="entity-list">
+                                        <li>
+                                            <span class="entity-list-title">Release:</span>July 21, 2014 (Dolby Theatre), August 1, 2014 (United States)</li>
+                                        <li>
+                                            <span class="entity-list-title">Directed:</span>
+                                            <a class="content-link" href="#">Lindson Wardens</a>,
+                                            <a class="content-link" href="#">Anabelle One</a>
+                                        </li>
+                                        <li>
+                                            <span class="entity-list-title">Starring:</span>
+                                            <a class="content-link" href="#">Octopus Wardens</a>,
+                                            <a class="content-link" href="#">Quanta Wardens</a>,
+                                            <a class="content-link" href="#">Anabelle Two</a>,
+                                            <a class="content-link" href="#">Anabelle Three</a>
+                                        </li>
+                                        <li>
+                                            <span class="entity-list-title">Production company:</span>
+                                            <a class="content-link" href="#">Re-Production Bro.</a>,
+                                            <a class="content-link" href="#">Pentakid</a>
+                                        </li>
+                                        <li>
+                                            <span class="entity-list-title">Country:</span>
+                                            <a class="content-link" href="#">USA</a>,
+                                            <a class="content-link" href="#">India</a>
+                                        </li>
+                                        <li>
+                                            <span class="entity-list-title">Language:</span>english</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="section-line">
+                            <div class="section-head">
+                                <h2 class="section-title text-uppercase">Synopsis</h2>
+                            </div>
+                            <div class="section-description">
+                                <p class="lead">Lead text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                                <h6 class="text-dark">Why do we use it?</h6>
+                                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                                <h6 class="text-dark">Where does it come from?</h6>
+                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+                                <p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
+                            </div>
+                            
+                        </div>
+                       
+                  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    
+  </body>
+</html>
